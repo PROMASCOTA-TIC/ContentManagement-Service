21467e6967c297a94c4e18f9a1b889eca8a00ac8
Rafaela Robalino <rafaela.robalino@epn.edu.ec>
2024-12-30 15:08:37 -0500

    chore: Configuraciones iniciales

--- apps/links-of-interest-ms/src/app/app.module.ts
+++ apps/links-of-interest-ms/src/app/app.module.ts
@@ -1,6 +1,33 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Dialect } from 'sequelize';
+import { envs } from '../config';
+import { Link } from './links-of-interest/models/link.models';
+import { Category } from './links-of-interest/models/category.model';
+import { LinksOfInterestController } from './links-of-interest/links-of-interest.controller';
+import { LinksOfInterestModule } from './links-of-interest/links-of-interest.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [],
+  imports: [
+    ScheduleModule.forRoot(),
+    LinksOfInterestModule,
+    SequelizeModule.forRoot({
+      dialect: envs.dbDialect as Dialect,
+      logging: console.log,
+      username: envs.dbLinksUsername,
+      password: envs.dbLinksPassword,
+      synchronize: true,
+      autoLoadModels: true, 
+      dialectOptions: {
+        connectString: envs.connectionString,
+      },
+      models: [Link, Category],
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
+
 export class AppModule {}
+console.log(envs);
