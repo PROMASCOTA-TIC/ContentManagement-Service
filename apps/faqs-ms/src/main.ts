e2ce6587f9d574c0386649199ca48be684f783d1
Rafaela Robalino <rafaela.robalino@epn.edu.ec>
2025-01-06 17:21:32 -0500

    feat: Actualizar configuración del microservicio y añadir nuevas dependencias

--- apps/faqs-ms/src/main.ts
+++ apps/faqs-ms/src/main.ts
@@ -1,10 +1,30 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    }
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  app.listen()
+
+  Logger.log(`Faqs Microservice is running`,'FaqsMicroservice');
 }
+
 bootstrap();
